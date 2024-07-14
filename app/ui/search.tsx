bad707223636413282c6e007ee61ce021777a156
Tim Matti <tmatti56@gmail.com>
2024-07-14 18:09:43 -0400

    implement invoice pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,7 +2,7 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { useSearchParams, usePathname, useRouter } from "next/navigation";
-import { useDebouncedCallback } from 'use-debounce';
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
@@ -10,10 +10,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const { replace } = useRouter();
 
   const handleSearch = useDebouncedCallback((term) => {
-    console.log(`Searching... ${term}`);
-
     const params = new URLSearchParams(searchParams);
-
+    params.set("page", "1");
     if (term) {
       params.set("query", term);
     } else {
